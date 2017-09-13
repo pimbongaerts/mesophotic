638bea725b0daea4610ff5d252898d264f56f844
Ryan Booker <ryanbooker@gmail.com>
2017-09-13 19:03:33 +1000

    Test searching

--- test/models/publication_test.rb
+++ test/models/publication_test.rb
@@ -1,6 +1,31 @@
 require 'test_helper'
 
 class PublicationTest < ActiveSupport::TestCase
+  test "search, find something" do
+    assert_equal (Publication.filter "II", ["title"], ["popular"]), (Publication.search "II", ["title"], ["popular"], true)
+    assert_equal (Publication.relevance "II", ["title"], ["popular"]), (Publication.search "II", ["title"], ["popular"], false)
+    assert_equal (Publication.relevance "II", ["title"], ["popular"]), (Publication.search "II", ["title"], ["popular"])
+    assert_equal (Publication.relevance "II"), (Publication.search "II")
+  end
+
+  test "search, find nothing" do
+    assert_equal [], (Publication.search "Fred Astaire", ["title"], ["popular"], true)
+    assert_equal [], (Publication.search "Ginger Rogers", ["title"], ["popular"], false)
+    assert_equal [], (Publication.search "James Dean", ["title"], ["popular"])
+    assert_equal [], (Publication.search "Marilyn Monroe")
+  end
+
+  test "empty search" do
+    assert_equal Publication.all.order(:id).to_a, (Publication.search "", ["title"], ["popular"], true).to_a
+    assert_equal Publication.all.order(:id).to_a, (Publication.search "", ["title"], ["popular"], false).to_a
+    assert_equal Publication.all.order(:id).to_a, (Publication.search "", ["title"], ["popular"]).to_a
+    assert_equal Publication.all.order(:id).to_a, (Publication.search "").to_a
+  end
+
+  test "search orders by id" do
+    assert_equal Publication.all.order(:id).to_a, (Publication.search "").to_a
+  end
+
   test "filter" do
     assert_equal [publications(:two)], (Publication.filter "II").to_a
   end
