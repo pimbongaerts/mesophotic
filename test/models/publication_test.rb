645aeabe38a697afda61a136e33c1b8bc395111b
Ryan Booker <ryanbooker@gmail.com>
2017-09-13 18:35:53 +1000

    Add a second test

--- test/models/publication_test.rb
+++ test/models/publication_test.rb
@@ -4,4 +4,8 @@ class PublicationTest < ActiveSupport::TestCase
   test "relevance" do
     assert_equal [publications(:two)], (Publication.relevance "II").to_a
   end
+
+  test "no relevance" do
+    assert_equal [], (Publication.relevance "Fred Astaire").to_a
+  end
 end
