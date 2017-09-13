ba24638c8058d31eef1fdaf4bdd28212312671ca
Ryan Booker <ryanbooker@gmail.com>
2017-09-13 18:53:01 +1000

    Test empty filter and relevance terms

--- test/models/publication_test.rb
+++ test/models/publication_test.rb
@@ -1,6 +1,18 @@
 require 'test_helper'
 
 class PublicationTest < ActiveSupport::TestCase
+  test "filter" do
+    assert_equal [publications(:two)], (Publication.filter "II").to_a
+  end
+
+  test "filtered everything" do
+    assert_equal [], (Publication.filter "Fred Astaire").to_a
+  end
+
+  test "no filter" do
+    assert_equal Publication.all, (Publication.filter "")
+  end
+
   test "relevance" do
     assert_equal [publications(:two)], (Publication.relevance "II").to_a
   end
@@ -11,15 +23,11 @@ class PublicationTest < ActiveSupport::TestCase
     assert_equal 1, scores.last.relevance
   end
 
-  test "no relevance" do
+  test "irrelevance" do
     assert_equal [], (Publication.relevance "Fred Astaire").to_a
   end
 
-  test "filter" do
-    assert_equal [publications(:two)], (Publication.filter "II").to_a
-  end
-
-  test "filtered everything" do
-    assert_equal [], (Publication.filter "Fred Astaire").to_a
+  test "no relevance" do
+    assert_equal Publication.all, (Publication.relevance "").to_a
   end
 end
