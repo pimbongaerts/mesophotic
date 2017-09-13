eb578375b5d4829afc0b92a6330d8788ecb6556b
Ryan Booker <ryanbooker@gmail.com>
2017-09-13 18:45:18 +1000

    Add filter tests, and relevance scores

--- test/models/publication_test.rb
+++ test/models/publication_test.rb
@@ -5,7 +5,21 @@ class PublicationTest < ActiveSupport::TestCase
     assert_equal [publications(:two)], (Publication.relevance "II").to_a
   end
 
+  test "relevance scores" do
+    scores = (Publication.relevance "Led Zeppelin")
+    assert_equal 2, scores.first.relevance
+    assert_equal 1, scores.last.relevance
+  end
+
   test "no relevance" do
     assert_equal [], (Publication.relevance "Fred Astaire").to_a
+  end
+
+  test "filter" do
+    assert_equal [publications(:two)], (Publication.filter "II").to_a
+  end
+
+  test "filtered everything" do
+    assert_equal [], (Publication.filter "Fred Astaire").to_a
   end
 end
