0f4e2415d7c4058ee0a39bfb6e985ab0a4f76c17
Pim Bongaerts <pim@uq.edu.au>
2018-02-07 15:26:22 +1000

    Fixed some more redirect_to :back

--- app/controllers/admin/platforms_controller.rb
+++ app/controllers/admin/platforms_controller.rb
@@ -2,6 +2,7 @@ class Admin::PlatformsController < Admin::BaseController
 	def destroy
 	    @publication = Publication.find(params[:publication_id])
 	    @publication.platforms.delete(params[:id])
-	    redirect_back, notice: 'Category was successfully removed.'
+	    redirect_back fallback_location: root_path, 
+	    			  notice: 'Category was successfully removed.'
 	end
 end
