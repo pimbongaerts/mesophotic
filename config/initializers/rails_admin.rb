e6849f068afbc21791907f875e06cd55d770b40e
Ryan Booker <ryanbooker@gmail.com>
2018-02-21 18:16:28 +1000

    Remove CanCan and update gems.

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -8,9 +8,6 @@ RailsAdmin.config do |config|
    end
   config.current_user_method(&:current_user)
 
-  ## == Cancan ==
-  config.authorize_with :cancan
-
   ## == Pundit ==
   # config.authorize_with :pundit
 
