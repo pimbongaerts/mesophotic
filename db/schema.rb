602e8737167e0b9e9d65f0c1ee166778f1442022
Ryan Booker <ryanbooker@gmail.com>
2018-09-28 17:49:05 +1000

    papertrail

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180426230042) do
+ActiveRecord::Schema.define(version: 2018_09_26_052926) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "commentable_id"
