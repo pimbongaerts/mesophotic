74e256cf401e933398d8b2f768f04e029ccba7a4
Ryan Booker <ryanbooker@gmail.com>
2019-04-03 17:41:36 +1000

    Flip bools from 1/0 to t/f

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_26_052926) do
+ActiveRecord::Schema.define(version: 2019_04_03_073938) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "commentable_id"
