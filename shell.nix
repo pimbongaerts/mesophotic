de2bb1d64a858cd21903dc3eb60e961cc6244789
Ryan Booker <7423+ryanbooker@users.noreply.github.com>
2020-12-17 16:30:02 +1000

    Add validations and mini_magick

--- shell.nix
+++ shell.nix
@@ -5,7 +5,6 @@ let
     ruby = ruby_2_5;
     gemdir = ./.;
   };
-in stdenv.mkDerivation {
-  name = "mesophotic";
+in mkShell {
   buildInputs = [ env env.wrappedRuby nodejs imagemagick ghostscript ];
 }
