91e30ae689afa408e441090fb897df3e8e622d7c
Ryan Booker <7423+ryanbooker@users.noreply.github.com>
2021-02-10 22:10:41 +1000

    Update sync script

--- sync_data_for_development.sh
+++ sync_data_for_development.sh
@@ -3,6 +3,4 @@ set -e
 set -o pipefail
 
 scp mesophotic:mesonew/db/production.sqlite3 db/development.sqlite3
-rsync -ralP mesophotic:mesonew/public/system public
-rsync -ralP mesophotic:mesonew/publications/pdfs publications
 rsync -ralP mesophotic:mesonew/storage .
