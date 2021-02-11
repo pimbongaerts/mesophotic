20d6ab4854e29d3fbe9c312b69572d95fea9847c
Ryan Booker <7423+ryanbooker@users.noreply.github.com>
2021-02-12 00:06:00 +1000

    Try to speed up and reduce memory of keywords

--- app/models/word_cloud.rb
+++ app/models/word_cloud.rb
@@ -1,32 +1,38 @@
 class WordCloud
   def self.generate size, content
-    exclusions = WordExclusion.pluck(:word)
-    species = Species.all.flat_map { |s| s.name.split(/\s/) }
+    exclusions = WordExclusion.pluck(:word).map(&:downcase).sort
+    species = Species.all.flat_map { |s| s.name.split(/\s/) }.map(&:downcase).sort
     
-    frequencies = content
-    .force_encoding("UTF-8")
-    .scan(/[\w']+/)
-    .reduce(Hash.new(0)) { |freqs, w|
-      word = normalize(species, w)
-      freqs[word] += 1 unless invalid(exclusions, word, w)
-      freqs
-    }
-    .sort_by { |k, v| v }
-    .reverse
-    .take(size)
+    frequencies = Hash.new(0)
+    
+    content
+      .force_encoding("UTF-8")
+      .scan(/[\w']+/)
+      .map { |word| [word, normalize(species, word)] }
+      .reject { |wn| 
+        invalid(exclusions, wn.first, wn.last) 
+      }
+      .map(&:last)
+      .each { |word| 
+        frequencies[word] += 1 
+      }
+    
+    frequencies = frequencies
+      .sort_by { |k, v| -v }
+      .take(size)
     
     max = frequencies.first.try(:last)
-    frequencies.map { |w| [w.first, w.last.to_f / max] }
+    frequencies.map { |f| [f.first, f.last.to_f / max] }
   end
 
-  def self.normalize species, w
-    species.include?(w) ? w.downcase : w.singularize.downcase
+  def self.normalize species, word
+    species.include?(word) ? word.downcase : word.singularize.downcase
   end
 
-  def self.invalid exclusions, word, w
-    exclusions.include? w or 
+  def self.invalid exclusions, word, norm
     exclusions.include? word or 
-    word.length <= 2 or 
-    word.numeric?
+    exclusions.include? norm or 
+    norm.length <= 2 or 
+    norm.numeric?
   end
 end
