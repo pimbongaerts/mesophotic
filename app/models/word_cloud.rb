78b6b9d50f4592f1929dfac4c2327045b0550767
Ryan Booker <7423+ryanbooker@users.noreply.github.com>
2021-02-12 00:41:02 +1000

    More memory saving, hopefully

--- app/models/word_cloud.rb
+++ app/models/word_cloud.rb
@@ -1,23 +1,15 @@
 class WordCloud
   def self.generate size, content
-    exclusions = WordExclusion.pluck(:word).map(&:downcase).sort
-    species = Species.all.flat_map { |s| s.name.split(/\s/) }.map(&:downcase).sort
+    exclusions = WordExclusion.select("LOWER(word) AS word").distinct.order(word: :asc).map(&:word)
+    species = Species.select("LOWER(name) AS name").distinct.order(name: :asc).flat_map { |s| s.name.split(/\s/) }
     
-    frequencies = Hash.new(0)
-    
-    content
+    frequencies = content
       .force_encoding("UTF-8")
       .scan(/[\w']+/)
-      .map { |word| [word, normalize(species, word)] }
-      .reject { |wn| 
-        invalid(exclusions, wn.first, wn.last) 
+      .each_with_object(Hash.new(0)) { |word, memo|
+        norm = normalize(species, word)
+        memo[word] += 1 unless invalid(exclusions, word, norm)
       }
-      .map(&:last)
-      .each { |word| 
-        frequencies[word] += 1 
-      }
-    
-    frequencies = frequencies
       .sort_by { |k, v| -v }
       .take(size)
     
@@ -30,9 +22,9 @@ class WordCloud
   end
 
   def self.invalid exclusions, word, norm
-    exclusions.include? word or 
-    exclusions.include? norm or 
-    norm.length <= 2 or 
+    exclusions.include?(word) || 
+    exclusions.include?(norm) || 
+    norm.length <= 2 || 
     norm.numeric?
   end
 end
