d53ff6bd3d96da2c9702f78257cead802647738a
Ryan Booker <7423+ryanbooker@users.noreply.github.com>
2023-02-10 09:09:28 +1000

    Add target dir to sync script, so a standalone backup can be made

--- scripts/sync_data_for_development.sh
+++ scripts/sync_data_for_development.sh
@@ -3,6 +3,8 @@ set -e
 set -o pipefail
 
 SCRIPT_DIR="${BASH_SOURCE[0]%/*}"
+DATA_DIR=${1:-"$SCRIPT_DIR/.."}
 
-rsync -ralP mesophotic:mesonew/db/production.sqlite3 "$SCRIPT_DIR"/../db/development.sqlite3
-rsync -ralP mesophotic:mesonew/storage "$SCRIPT_DIR"/..
+mkdir -p "$DATA_DIR"/db
+rsync -ralP mesophotic:mesonew/db/production.sqlite3 "$DATA_DIR"/db/development.sqlite3
+rsync -ralP mesophotic:mesonew/storage "$DATA_DIR"
