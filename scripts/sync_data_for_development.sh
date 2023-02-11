741a2debf7b41aa7a916b149bcfcc4b779a8c336
Ryan Booker <7423+ryanbooker@users.noreply.github.com>
2023-02-11 16:23:27 +1000

    Update sync script

--- scripts/sync_data_for_development.sh
+++ scripts/sync_data_for_development.sh
@@ -6,5 +6,5 @@ SCRIPT_DIR="${BASH_SOURCE[0]%/*}"
 DATA_DIR=${1:-"$SCRIPT_DIR/.."}
 
 mkdir -p "$DATA_DIR"/db
-rsync -ralP mesophotic:mesonew/db/production.sqlite3 "$DATA_DIR"/db/development.sqlite3
-rsync -ralP mesophotic:mesonew/storage "$DATA_DIR"
+rsync -aP mesophotic:mesonew/db/production.sqlite3 "$DATA_DIR"/db/development.sqlite3
+rsync -aP mesophotic:mesonew/storage "$DATA_DIR"
