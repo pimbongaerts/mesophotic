c1518f83ccf9c3323a84e8e5bd3fc67e88d74f52
Ryan Booker <7423+ryanbooker@users.noreply.github.com>
2023-02-26 18:43:37 +1000

    Update sync script to leave a copy of production.sqlite3

--- scripts/sync_data_for_development.sh
+++ scripts/sync_data_for_development.sh
@@ -6,5 +6,6 @@ SCRIPT_DIR="${BASH_SOURCE[0]%/*}"
 DATA_DIR=${1:-"$SCRIPT_DIR/.."}
 
 mkdir -p "$DATA_DIR"/db
-rsync -aP mesophotic:mesonew/db/production.sqlite3 "$DATA_DIR"/db/development.sqlite3
+rsync -aP mesophotic:mesonew/db/production.sqlite3 "$DATA_DIR"/db/production.sqlite3
+cp "$DATA_DIR"/db/production.sqlite3 "$DATA_DIR"/db/development.sqlite3
 rsync -aP mesophotic:mesonew/storage "$DATA_DIR"
