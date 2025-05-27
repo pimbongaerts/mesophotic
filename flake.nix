c256dddef39aa38b40ee7cc6a7e8af72fac933de
Ryan Booker <7423+ryanbooker@users.noreply.github.com>
2025-05-27 14:33:55 +1000

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,15 +1,42 @@
 {
-  description = "Mesophotic development environment";
+  description = "Mesophotic Rails development environment";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs-ruby.url = "github:bobvanderlinden/nixpkgs-ruby";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in {
-          devShells.default = import ./shell.nix { inherit pkgs; };
-        }
-      );
+  outputs = { self, nixpkgs, nixpkgs-ruby, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ nixpkgs-ruby.overlays.default ];
+        };
+
+        rubyEnv = pkgs.bundlerEnv {
+          name = "mesophotic-dev-env";
+          ruby = pkgs."ruby-2.7";
+          gemdir = ./.;
+        };
+
+        shellBuildInputs = [
+          pkgs.awscli
+          rubyEnv
+          rubyEnv.wrappedRuby
+          pkgs.imagemagick
+          pkgs.ghostscript
+          pkgs.mupdf
+          pkgs.v8
+        ];
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = shellBuildInputs;
+          };
+        };
+      }
+    );
 }
