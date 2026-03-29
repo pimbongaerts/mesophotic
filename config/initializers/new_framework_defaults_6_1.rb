6976738740c663a18cc44339ab2bf6620f2b054f
Ryan Booker <7423+ryanbooker@users.noreply.github.com>
2026-03-29 17:17:37 +1000

    Add Rails 6.1 framework defaults and permissions_policy initializers, remove deprecated config

---
+++ config/initializers/new_framework_defaults_6_1.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+#
+# This file eases your Rails 6.1 framework defaults upgrade.
+#
+# All defaults below are already active via config.load_defaults 6.1
+# in config/application.rb. This file is kept for documentation only.
+#
+# Read the Rails 6.1 upgrade guide for more information on each option:
+# https://guides.rubyonrails.org/upgrading_ruby_on_rails.html#upgrading-from-rails-6-0-to-rails-6-1
