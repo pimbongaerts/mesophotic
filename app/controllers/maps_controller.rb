e88ee5276ccb0e33645bad88a8af04043192ac03
Ryan Booker <7423+ryanbooker@users.noreply.github.com>
2026-03-31 10:01:42 +1000

    Cache world map endpoint: keyed on model, IDs, and Publication.updated_at

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,12 +1,14 @@
 class MapsController < ApplicationController
   def world_map
-    data = params[:model].singularize.classify.constantize.where(id: params[:ids].split(','))
-
-    render partial: 'shared/world_map_clickable',
-           locals: { title: params[:model].pluralize.titleize,
-                     data: get_coordinates(data, params[:z]),
-                     backgroundcolor: params[:backgroundcolor],
-                     height: params[:height] }
+    cached = Rails.cache.fetch(["world_map", params[:model], params[:ids], params[:z], Publication.maximum(:updated_at)]) do
+      data = params[:model].singularize.classify.constantize.where(id: params[:ids].split(','))
+      render_to_string partial: 'shared/world_map_clickable',
+             locals: { title: params[:model].pluralize.titleize,
+                       data: get_coordinates(data, params[:z]),
+                       backgroundcolor: params[:backgroundcolor],
+                       height: params[:height] }
+    end
+    render html: cached.html_safe
   end
 
   private
