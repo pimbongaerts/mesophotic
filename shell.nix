ac15d942351f1597907dcfbd2d29494eed8afb1e
Ryan Booker <7423+ryanbooker@users.noreply.github.com>
2026-04-03 09:22:24 +1000

    Update nix scripts to reference ruby 3.2

--- shell.nix
+++ shell.nix
@@ -3,7 +3,7 @@
 , nixpkgs-ruby ? import (builtins.fetchTarball {
     url = "https://github.com/bobvanderlinden/nixpkgs-ruby/archive/c1ba161adf31119cfdbb24489766a7bcd4dbe881.tar.gz";
   })
-, ruby ? nixpkgs-ruby.packages.${builtins.currentSystem}."ruby-2.7"
+, ruby ? nixpkgs-ruby.packages.${builtins.currentSystem}."ruby-3.2"
 , bundler ? nixpkgs-ruby.packages.${builtins.currentSystem}."bundler"
 }:
 pkgs.mkShell {
