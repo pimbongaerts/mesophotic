b4836f334d24fa091450e815a44b338269ff2220
Ryan Booker <7423+ryanbooker@users.noreply.github.com>
2026-04-03 23:24:27 +1000

    Update 6.1 framework defaults file to documentation-only

--- config/initializers/new_framework_defaults_6_1.rb
+++ config/initializers/new_framework_defaults_6_1.rb
@@ -1,9 +1,5 @@
-# Be sure to restart your server when you modify this file.
-#
-# This file eases your Rails 6.1 framework defaults upgrade.
-#
-# All defaults below are already active via config.load_defaults 6.1
+# All Rails 6.1 framework defaults are now active via config.load_defaults 7.0
 # in config/application.rb. This file is kept for documentation only.
 #
-# Read the Rails 6.1 upgrade guide for more information on each option:
-# https://guides.rubyonrails.org/upgrading_ruby_on_rails.html#upgrading-from-rails-6-0-to-rails-6-1
+# See config/initializers/new_framework_defaults_7_0.rb for 7.0 defaults
+# that are still being migrated.
