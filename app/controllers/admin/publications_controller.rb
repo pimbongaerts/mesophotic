cf2c5c4fbc303a127b78f22fe158aea7a236820f
Ryan Booker <7423+ryanbooker@users.noreply.github.com>
2026-04-04 22:37:22 +1000

    Remove stale callback action references across controllers (Rails 8.0 enforces)

--- app/controllers/admin/publications_controller.rb
+++ app/controllers/admin/publications_controller.rb
@@ -1,15 +1,7 @@
 class Admin::PublicationsController < Admin::BaseController
   before_action :require_admin!
-  before_action :set_post, only: [
-    :edit,
-    :update,
-    :destroy
-  ]
 
   def dashboard
 	  @publications = Publication.includes(:users, :journal, pdf_attachment: :blob).page(params[:page])
-  end
-
-  def destroy
   end
 end
