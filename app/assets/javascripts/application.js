0988df04d610971e550b2fd10c7ec04e22c29b38
Ryan Booker <7423+ryanbooker@users.noreply.github.com>
2026-04-04 13:25:18 +1000

    Remove rails-ujs, convert method:/confirm: to turbo_method:/turbo_confirm:

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
 //= require bootstrap5
 //= require dual_range
 //= require wordcloud2
