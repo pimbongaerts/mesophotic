2d271f1a8147955e7242fe0042c71c61161e077f
Ryan Booker <7423+ryanbooker@users.noreply.github.com>
2026-04-05 15:59:21 +1000

    RailsAdmin: switch to importmap + vendored CSS (nobuild approach)

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,3 +2,6 @@
 #
 # Propshaft serves all files in asset paths automatically.
 # dartsass-rails compiles SCSS to app/assets/builds/.
+
+# Add vendor stylesheets path for RailsAdmin pre-compiled CSS.
+Rails.application.config.assets.paths << Rails.root.join("vendor/assets/stylesheets")
